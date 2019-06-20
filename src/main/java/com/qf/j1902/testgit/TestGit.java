1a37b5c71db9d04d555c2d23eaab532fc47738b6
justgoYML <1660473326@qq.com>
2019-06-20 14:49:06 +0800

    TestGit

---
+++ src/main/java/com/qf/j1902/testgit/TestGit.java
@@ -0,0 +1,14 @@
+package com.qf.j1902.testgit;
+
+import lombok.Data;
+import org.apache.http.auth.UsernamePasswordCredentials;
+
+/**
+ * Created by Administrator on 2019/6/20.
+ */
+@Data
+public class TestGit {
+    private int id;
+    private String username;
+    private String password;
+}
